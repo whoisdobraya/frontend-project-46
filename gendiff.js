cee895cd50d1813e4faf535601de739fa53c2b4d
maria <aa@aa>
2026-03-29 15:31:34 +0300

    added -f and argument

--- gendiff.js
+++ gendiff.js
@@ -5,8 +5,10 @@ const program = new Command();
 
 program
     .name('gendiff')
+    .argument('<filepath1> <filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version')
-    .helpOption('-h, --help', 'display help for command');
+    .helpOption('-h, --help', 'display help for command')
+    .option('-f, --format [type]', 'output format')
 
 program.parse();
